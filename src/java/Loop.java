58e456d0eccf448dcb6715c084f2ce26277462d6
Gunmaykharabe <gvak925@gmail.com>
2023-08-18 16:15:14 +0530

    Loop program

---
+++ src/java/Loop.java
@@ -0,0 +1,20 @@
+public class Loop{
+	public static void main(String[] arg){
+		
+		//loop
+		
+		//1.for loop
+		
+		//for(decleration-initialization ; condition ; incre/decre){
+		
+	//}
+	
+	int range = 10;
+	
+	for(int i =1 ; i<=10 ; i++){
+		System.out.println("      "+i);
+	}
+		//2.while loop
+}
+
+}
