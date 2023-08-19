f1b43637d4f30d8ed4b158b4504b03803d21fe65
Gunmaykharabe <gvak925@gmail.com>
2023-08-19 12:28:26 +0530

    Array program

--- src/java/Array.java
+++ src/java/Array.java
@@ -2,7 +2,11 @@ public class Array{
 	public static void main(String[] arg){
 		String a="gunmay";
 		String name[]={"gunmay","chinmay","ganesh","gaurav"};
-		System.out.println(name[0]);
+		int number[] ={23,49,46,38};
+		System.out.println(number[3]);
+		for(int i = 0; i<=number.length; i++){
+			System.out.println(number[i]);
+	}
 	}
 	
 }
